25a7ec9ae6afbbafc9c789f43b0903155a943e87
crazyquark <cristian.sandu@gmail.com>
2021-10-15 13:30:50 +0300

    add test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,144 +1,194 @@
-/*      __  __                ____       __            __            
- *     / / / /___ __  ___  __/ __ \___  / /____  _____/ /_____  _____
- *    / /_/ / __ `/ |/_/ |/_/ / / / _ \/ __/ _ \/ ___/ __/ __ \/ ___/
- *   / __  / /_/ />  <_>  </ /_/ /  __/ /_/  __/ /__/ /_/ /_/ / /    
- *  /_/ /_/\__,_/_/|_/_/|_/_____/\___/\__/\___/\___/\__/\____/_/     
- * 
- *  A simple deauth + dissassociation attack detector written for the WiFi Nugget
- *  github.com/crazyquark/HaxxDetector
- * 
- *  By Alex Lynd | alexlynd.com
- * 
- *  Hacked modestly by @crazyquark
- *  to run on any ESP8266 device with an OLED screen. 
- */
+/**
+   The MIT License (MIT)
 
-#include <ESP8266WiFi.h>
-#include <Wire.h>
-#include <SSD1306Wire.h>
-#include <SSD1306Brzo.h>
+   Copyright (c) 2018 by ThingPulse, Daniel Eichhorn
+   Copyright (c) 2018 by Fabrice Weinberg
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+
+   ThingPulse invests considerable time and money to develop these open source libraries.
+   Please support us by buying our products (and not the clones) from
+   https://thingpulse.com
+
+*/
+
+// Include the correct display library
+// For a connection via I2C using Wire include
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include <SSD1306Wire.h> // legacy include: `#include "SSD1306.h"`
+// or #include "SH1106Wire.h", legacy include: `#include "SH1106.h"`
+// For a connection via I2C using brzo_i2c (must be installed) include
+// #include <brzo_i2c.h> // Only needed for Arduino 1.6.5 and earlier
+// #include "SSD1306Brzo.h"
+// #include "SH1106Brzo.h"
+// For a connection via SPI include
+// #include <SPI.h> // Only needed for Arduino 1.6.5 and earlier
+// #include "SSD1306Spi.h"
+// #include "SH1106SPi.h"
+
+// Include the UI lib
 #include <OLEDDisplayUi.h>
 
-#include "nuggs.h" // Nugget Face bitmap files
+// Include custom images
 
-SSD1306Wire display(0x3c, D1, D2); // initialize OLED on I2C pins
-OLEDDisplayUi ui(&display);
+#include "images.h"
 
-extern "C"
-{
-#include "user_interface.h"
+// Use the corresponding display class:
+
+// Initialize the OLED display using SPI
+// D5 -> CLK
+// D7 -> MOSI (DOUT)
+// D0 -> RES
+// D2 -> DC
+// D8 -> CS
+// SSD1306Spi        display(D0, D2, D8);
+// or
+// SH1106Spi         display(D0, D2);
+
+// Initialize the OLED display using brzo_i2c
+// D3 -> SDA
+// D5 -> SCL
+// SSD1306Brzo display(0x3c, D3, D5);
+// or
+// SH1106Brzo  display(0x3c, D3, D5);
+
+// Initialize the OLED display using Wire library
+SSD1306Wire display(0x3d, D1, D2);  // ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically based on your board's pins_arduino.h e.g. https://github.com/esp8266/Arduino/blob/master/variants/nodemcu/pins_arduino.h
+// SH1106Wire display(0x3c, SDA, SCL);
+
+OLEDDisplayUi ui     ( &display );
+
+void msOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
+  display->setTextAlignment(TEXT_ALIGN_RIGHT);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(128, 0, String(millis()));
 }
 
-const short channels[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}; // Max: US 11, EU 13, JAP 14
+void drawFrame1(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  // draw an xbm image.
+  // Please note that everything that should be transitioned
+  // needs to be drawn relative to x and y
 
-int ch_index{0};
-int packet_rate{0};
-int attack_counter{0};
-unsigned long update_time{0};
-unsigned long ch_time{0};
-
-void sniffer(uint8_t *buf, uint16_t len)
-{
-  if (!buf || len < 28)
-    return;
-  byte pkt_type = buf[12];
-
-  if (pkt_type == 0xA0 || pkt_type == 0xC0)
-  { // flag deauth & dissassociation frames
-    ++packet_rate;
-  }
+  display->drawXbm(x + 34, y + 14, WiFi_Logo_width, WiFi_Logo_height, WiFi_Logo_bits);
 }
 
+void drawFrame2(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  // Demonstrates the 3 included default sizes. The fonts come from SSD1306Fonts.h file
+  // Besides the default fonts there will be a program to convert TrueType fonts into this format
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(0 + x, 10 + y, "Arial 10");
 
-void displayDeadNugg()
-{
-  display.clear();
-  display.drawXbm(0, 0, alive_nugg_width, alive_nugg_height, dead_nugg);
-  display.display();
+  display->setFont(ArialMT_Plain_16);
+  display->drawString(0 + x, 20 + y, "Arial 16");
+
+  display->setFont(ArialMT_Plain_24);
+  display->drawString(0 + x, 34 + y, "Arial 24");
 }
 
-void displayAliveNugg()
-{
-  display.clear();
-  display.drawXbm(0, 0, alive_nugg_width, alive_nugg_height, alive_nugg);
-  display.display();
+void drawFrame3(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  // Text alignment demo
+  display->setFont(ArialMT_Plain_10);
+
+  // The coordinates define the left starting point of the text
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->drawString(0 + x, 11 + y, "Left aligned (0,10)");
+
+  // The coordinates define the center of the text
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->drawString(64 + x, 22 + y, "Center aligned (64,22)");
+
+  // The coordinates define the right end of the text
+  display->setTextAlignment(TEXT_ALIGN_RIGHT);
+  display->drawString(128 + x, 33 + y, "Right aligned (128,33)");
 }
 
-void attack_started()
-{
-  // pixels.setPixelColor(0, pixels.Color(150, 0, 0));
-  // pixels.show(); // red
-  displayDeadNugg();
+void drawFrame4(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  // Demo for drawStringMaxWidth:
+  // with the third parameter you can define the width after which words will be wrapped.
+  // Currently only spaces and "-" are allowed for wrapping
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(ArialMT_Plain_10);
+  display->drawStringMaxWidth(0 + x, 10 + y, 128, "Lorem ipsum\n dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore.");
 }
 
-void attack_stopped()
-{
-  // pixels.setPixelColor(0, pixels.Color(0, 150, 0));
-  // pixels.show(); // green
-  displayAliveNugg();
+void drawFrame5(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+
 }
 
-void setup()
-{
-  ui.setTargetFPS(60);
-  ui.init(); // initialize OLED screen
+// This array keeps function pointers to all frames
+// frames are the single views that slide in
+FrameCallback frames[] = { drawFrame1, drawFrame2, drawFrame3, drawFrame4, drawFrame5 };
 
-  // initalize WiFi card for scanning
-  WiFi.disconnect();
-  wifi_set_opmode(STATION_MODE);
-  wifi_set_promiscuous_rx_cb(sniffer);
-  wifi_set_channel(1);
-  wifi_promiscuous_enable(true);
+// how many frames are there?
+int frameCount = 5;
 
-  Serial.begin(9600);
+// Overlays are statically drawn on top of a frame eg. a clock
+OverlayCallback overlays[] = { msOverlay };
+int overlaysCount = 1;
 
+void setup() {
+  Serial.begin(115200);
   Serial.println();
-  Serial.println("   __ __                ___      __          __          ");
-  Serial.println("  / // /__ ___ ____ __ / _ \\___ / /____ ____/ /____  ____");
-  Serial.println(" / _  / _ `/\\ \\ /\\ \\ // // / -_) __/ -_) __/ __/ _ \/ __/");
-  Serial.println("/_//_/\_,_//_\\_\\/_\\_\\/____/\\__/\\__/\\__/\\__/\\__/\\___/_/  ");
-  Serial.println("\ngithub.com/HakCat-Tech/HaxxDetector");
-  Serial.println("A WiFi Nugget sketch by Alex Lynd");
+  Serial.println();
 
-  display.clear();
+  // The ESP is capable of rendering 60fps in 80Mhz mode
+  // but that won't give you much time for anything else
+  // run it in 160Mhz mode or just set it to 30 fps
+  ui.setTargetFPS(60);
+
+  // Customize the active and inactive symbol
+  ui.setActiveSymbol(activeSymbol);
+  ui.setInactiveSymbol(inactiveSymbol);
+
+  // You can change this to
+  // TOP, LEFT, BOTTOM, RIGHT
+  ui.setIndicatorPosition(BOTTOM);
+
+  // Defines where the first frame is located in the bar.
+  ui.setIndicatorDirection(LEFT_RIGHT);
+
+  // You can change the transition that is used
+  // SLIDE_LEFT, SLIDE_RIGHT, SLIDE_UP, SLIDE_DOWN
+  ui.setFrameAnimation(SLIDE_LEFT);
+
+  // Add frames
+  ui.setFrames(frames, frameCount);
+
+  // Add overlays
+  ui.setOverlays(overlays, overlaysCount);
+
+  // Initialising the UI will init the display too.
+  ui.init();
+
   display.flipScreenVertically();
-  // pixels.setPixelColor(0, pixels.Color(0, 150, 0));
-  // pixels.show();
-  displayAliveNugg();
+
 }
 
-void loop()
-{
-  unsigned long current_time = millis();
 
-  if (current_time - update_time >= (sizeof(channels) * 100))
-  {
-    update_time = current_time;
+void loop() {
+  int remainingTimeBudget = ui.update();
 
-    if (packet_rate >= 1)
-    {
-      ++attack_counter;
-    }
-    else
-    {
-      if (attack_counter >= 1)
-        attack_stopped();
-      attack_counter = 0;
-    }
-
-    if (attack_counter == 1)
-    {
-      attack_started();
-    }
-    packet_rate = 0;
-  }
-
-  // Channel hopping
-  if (sizeof(channels) > 1 && current_time - ch_time >= 100)
-  {
-    ch_time = current_time; // Update time variable
-    ch_index = (ch_index + 1) % (sizeof(channels) / sizeof(channels[0]));
-    short ch = channels[ch_index];
-    wifi_set_channel(ch);
+  if (remainingTimeBudget > 0) {
+    // You can do some work here
+    // Don't do stuff if you are below your
+    // time budget.
+    delay(remainingTimeBudget);
   }
 }
